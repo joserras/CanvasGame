c9400587cdd501901ec7babbf56e73285d7c5eff
joserras <diego_lucas_1995@hotmail.com>
2018-07-02 19:25:21 +0200

    connect

--- public/js/index.js
+++ public/js/index.js
@@ -51,7 +51,7 @@ function myTimer() {
 //http://battleshipa.herokuapp.com/
 //Envía el nombre de usuario y su socket.id al servidor
 function login(data){
-    socket = io.connect('http://battleshipa.herokuapp.com/', { 'forceNew': true });
+    socket = io.connect('https://battleshipa.herokuapp.com/', { 'forceNew': true });
     var usuario = data.nickname.value;
     loading();
     activateSockets();
